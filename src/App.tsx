7f427ddcbc562b0ff6eb0fa7849c7d2cdef2f118
lalinthorn <lalinpye@gmail.com>
2021-09-09 14:42:24 +0700

    1

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,33 @@
 import React from 'react';
 import logo from './logo.svg';
-import Post from './data/post';
+import './App.css';
 
 function App() {
   return (
     <div className="p-2">
+      {/* post container */}
       <div className="mx-auto max-w-2xl bg-gray-800 rounded-md shadow p-4 space-y-2 divide-y">
-        <Post></Post>
+
+        {/* ส่วนของเจ้าของ post */}
+        <div className="space-y-2">
+
+          {/* image and name */}
+          <div className="flex space-x-2 items-center">
+            <img className="w-12 h-12 rounded-full" src="/profileImages/p.jpg"></img>
+            <span className='font-semibold text-lg text-white'>lalinthorn pholnaruk 630610758</span>
+          </div>
+
+          {/* status message */}
+          <p className='text-white'>Quiz ง่ายจังเลยครับ ขอยาก ๆ กว่านี้ได้ไหม #261207</p>
+
+          {/* like section */}
+          <div className='flex items-center'>
+            <img className='w-4 h-4 mr-1' src='/like.svg'></img>
+            <p className='text-gray-300'>100 คน</p>
+          </div>
+
+        </div>
+
         {/* comments section */}
         <div className="">
 
@@ -33,11 +54,32 @@ function App() {
               <p className='text-white'>เม้นค้าบ</p>
             </div>
           </div>
+          <div className="flex p-2 items-start space-x-2 pl-14">
+            <img className="w-10 w-10 rounded-full" src="/profileImages/popcat.png"></img>
+            <div className="bg-gray-500 rounded-lg p-2">
+              <p className="font-semibold text-white">แมวตัวหนึ่ง</p>
+              <p className='text-white'>ลิซ่าาาาาาา</p>
+              <div className='flex items-center'>
+                <img className='w-4 h-4 mr-1' src='/like.svg'></img>
+                <p className='text-gray-300'>2 คน</p>
+              </div>
+            </div>
+          </div>
+
+          <div className="flex p-2 items-start space-x-2">
+            <img className="w-10 w-10 rounded-full" src="/profileImages/charliebrown.jpg"></img>
+            <div className="bg-gray-500 rounded-lg p-2">
+              <p className="font-semibold text-white">Charlie Brown</p>
+              <p className='text-white'>บ้าไปแล้ว</p>
+              {/* like section (จะไม่แสดงถ้าไม่มีใครไลค์เลย) */}
+              <div className='flex items-center'>
+                <img className='w-4 h-4 mr-1' src='/like.svg'></img>
+                <p className='text-gray-300'>207 คน</p>
+              </div>
+            </div>
+          </div>
 
         </div>
-
-
-
 
       </div>
     </div>
