ad647f1d410a7831d7ab261a70cc14f2564950e2
lalinthorn <lalinpye@gmail.com>
2021-09-09 14:57:39 +0700

    1

--- src/App.tsx
+++ src/App.tsx
@@ -51,7 +51,7 @@ function App() {
             <img className="w-10 w-10 rounded-full" src="/profileImages/puppy.jpg"></img>
             <div className="bg-gray-500 rounded-lg p-2">
               <p className="font-semibold text-white">หมาน้อย</p>
-              <p className='text-white'>เม้นค้าบ</p>
+              <p className='text-white'>จริงค้าบบบบบบบบ</p>
             </div>
           </div>
           <div className="flex p-2 items-start space-x-2 pl-14">
